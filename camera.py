61132c9554e17f3658ea3128bd667c5758b02bba
Szymon-Groch <szymon.groch20@gmail.com>
2024-06-05 15:54:03 +0200

    Update camera.py
    translating

--- camera.py
+++ camera.py
@@ -1,54 +1,48 @@
 from ultralytics import YOLO
 import cv2
-import math 
+import math
 
-# wybór kamerki i rozdzielczości przechwytywanego obrazu
+# Choosing the camera and setting the resolution of the captured image
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-
 model = YOLO("znaki.pt")
 
-
-znaki = ['agatka','autostrada','ograniczenie_predkosci','parking','pierszenstwo','prosto','przejscie_pieszych','stop','ustap'] 
-
+signs = ['agatka', 'autostrada', 'speed_limit', 'parking', 'priority', 'straight', 'pedestrian_crossing', 'stop', 'yield'] 
 
 while True:
     success, img = cap.read()
-    wyniki = model(img, stream=True)
+    results = model(img, stream=True)
 
+    for r in results:
+        boxes = r.boxes
 
-    for r in wyniki:
-        ramki = r.boxes
+        for box in boxes:
+            # Defining bounding box coordinates
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
-        for ramka in ramki:
-            # definowanie oznaczającej ramki 
-            x1, y1, x2, y2 = ramka.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) 
-
-            # umieszczenie ramki w obrazie z kamery
+            # Placing bounding box on the camera image
             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 0), 3)
 
-            # pewność identyfikacji znaku
-            confidence = math.ceil((ramka.conf[0]*100))/100
-            print("Confidence --->",confidence)
+            # Confidence of sign identification
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+            print("Confidence --->", confidence)
 
-            # nazwa znaku
-            znak = int(ramka.cls[0])
-            print("Nazwa znaku -->", znaki[znak])
+            # Sign name
+            sign = int(box.cls[0])
+            print("Sign name -->", signs[sign])
 
-            
             org = [x1, y1]
             font = cv2.FONT_HERSHEY_SIMPLEX
             fontScale = 1
-            
             color = (255, 0, 0)
             thickness = 2
-            detection = znaki[znak] + " " + str(confidence)
-            cv2.putText(img, detection, org, font, fontScale, color, thickness)
+            detection = signs[sign] + " " + str(confidence)
+            cv2.putText(img, detection, tuple(org), font, fontScale, color, thickness)
 
-    cv2.imshow('Kamerka', img)
+    cv2.imshow('Camera', img)
     if cv2.waitKey(1) == ord('q'):
         break
 
